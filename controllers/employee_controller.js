fd72b984ef1e77f0dc37f64544e21744e8ad788a
Shashank Naik <shashanknaik808@gmail.com>
2023-10-05 16:27:04 +0530

    Retrieving List of Employees

--- controllers/employee_controller.js
+++ controllers/employee_controller.js
@@ -1,9 +1,12 @@
 const express = require('express'),
     router = express.Router()
 
+const db = require('../db.js');
 
 router.get('/', (req, res) => {
-    res.send('List of employees');
+    db.query("SELECT * FROM employees")
+        .then(data => res.send(data))
+        .catch(err => console.log(err))
 })
 
 module.exports = router;
