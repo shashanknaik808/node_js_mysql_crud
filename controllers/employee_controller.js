939aa25ae5951726215e770425745134d946e8ee
Shashank Naik <shashanknaik808@gmail.com>
2023-10-05 16:58:43 +0530

    Retrieving a Record with Given Id - getEmployeeById

--- controllers/employee_controller.js
+++ controllers/employee_controller.js
@@ -8,6 +8,17 @@ const service = require('../services/employee_service.js');
 router.get('/', async (req, res) => {
     const employees = await service.getAllEmployees()
     res.send(employees)
-})
+});
+
+
+router.get('/:id', async (req, res) => {
+
+    const employee = await service.getEmployeeById(req.params.id)
+
+    if (employee == undefined)
+        res.status(404).json('no record with given id : ' + req.params.id)
+    else
+        res.send(employee)
+});
 
 module.exports = router;
--- services/employee_service.js
+++ services/employee_service.js
@@ -1,8 +1,15 @@
 const db = require('../db.js');
 
 module.exports.getAllEmployees = async (req, res) => {
-    
-    const [rows] = await db.query("SELECP * FROM employees")
+
+    const [rows] = await db.query("SELECT * FROM employees")
         .catch(err => console.log(err))
     return rows;
-}
+};
+
+
+module.exports.getEmployeeById = async (id) => {
+
+    const [[record]] = await db.query("SELECT * FROM employees WHERE id = ?", [id])
+    return record;
+};
